4fc23420f35242951c456ef6215ff81b4cc865a3
虎子哥 <flashindream@gmail.com>
2019-11-22 18:12:09 +0800

    add examples

---
+++ lib/mBuild/examples/dualrgbcolor.cpp
@@ -0,0 +1,33 @@
+#include <Arduino.h>
+#include "mBuild.h"
+mDualRGBColor dualRGB;
+void colorHandle(DualRGBColor color){
+    String msg = "color:";
+    msg += color.r1;
+    msg += " ";
+    msg += color.g1;
+    msg += " ";
+    msg += color.b1;
+    msg += " ";
+    msg += color.r2;
+    msg += " ";
+    msg += color.g2;
+    msg += " ";
+    msg += color.b2;
+    msg += " ";
+    msg += " freeMemory:";
+    msg += ESP.getFreeHeap();
+    mBuild::log(msg);
+}
+void setup()
+{
+    mBuild::init();
+    mBuild::broadcast();
+    delay(1000);
+    dualRGB.begin(0x1);
+}
+void loop()
+{
+    dualRGB.getRGB(&colorHandle);
+    delay(1000);
+}
